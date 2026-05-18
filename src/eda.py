faa6496a6a5a5573f509507c93088668b92d316c
levy <levyfelipesal@gmail.com>
2026-05-18 09:37:27 -0300

    Add option to plot in English; add option to save only, not plot

--- src/eda.py
+++ src/eda.py
@@ -9,7 +9,8 @@ sns.set_theme(context='notebook', style='ticks')
 plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['mathtext.fontset'] = 'cm'
 
-def cramerv_inputs_target(df, target_column=-1, style_format='standard', significance=0.05, save=False, path='chi2-test_result.csv'):
+def cramerv_inputs_target(df, target_column=-1, style_format='standard', significance=0.05, save=False, path='chi2-test_result.csv',
+                          display_language='pt-br'):
     """
     Exibe tabela contendo o V de Cramér entre a saída e as entradas categóricas, bem como valores importantes do teste Chi2 de associação.
     
@@ -36,6 +37,14 @@ def cramerv_inputs_target(df, target_column=-1, style_format='standard', signifi
     d = df.shape[1]
     if type(target_column) == int:
         target_column = df.columns[target_column]
+
+    if display_language=='pt-br':
+        df_caption = "V de Cramér entre entradas e a variável '"+target_column+"'"
+    elif display_language=='en':
+        df_caption = "Cramer's V between inputs and the variable '"+target_column+"'"
+    else:
+        raise NotImplementedError
+    
     if style_format == 'standard':
         style_format = {'V':lambda x:ffp(x, 4, min_digits=4),
                         'Chi2': lambda x:ffp(x, 2),
@@ -63,9 +72,7 @@ def cramerv_inputs_target(df, target_column=-1, style_format='standard', signifi
 
     display(contingency_df.sort_values(
         by='V', ascending=False
-        ).iloc[1:].style.set_caption(
-            "V de Cramér entre entradas e a variável '"+target_column+"'"
-            ).format(style_format)
+        ).iloc[1:].style.set_caption(df_caption).format(style_format)
             )
 
 def plot_histogram(df, feature_name, save=False, path='histogram.png'):
@@ -92,7 +99,7 @@ def plot_histogram(df, feature_name, save=False, path='histogram.png'):
         fig.savefig(path, dpi=300)
     plt.show()
 
-def plot_boxplot(df, feature_name, save=False, path='boxplot.png'):
+def boxplot(df, feature_name, save=False, path='boxplot.png', plot_language='pt-br', plot=True):
     """
     Plota dois boxplots personalizados: 1) da variável numérica em um geral e 2) condicionado pela classe de saída.
 
@@ -102,6 +109,13 @@ def plot_boxplot(df, feature_name, save=False, path='boxplot.png'):
         save (bool, optional): Indica se o boxplot deve ser salvo;
         path (str, optional): Caminho completo de armazenamento.
     """
+    if plot_language=='pt-br':
+        ax_title = ['Geral', 'Condicionado pelas classes de saída']
+    elif plot_language=='en':
+        ax_title = ['General', 'Conditioned by the output classes']
+    else:
+        raise NotImplementedError
+        
     fig, ax = plt.subplots(1, 2, figsize=(8, 4), layout='constrained')
     sns.boxplot(ax=ax[0],
                 data=df, y=feature_name,
@@ -111,15 +125,17 @@ def plot_boxplot(df, feature_name, save=False, path='boxplot.png'):
                 color=".8", linecolor="#137", linewidth=.75, width=.2, flierprops={'marker':'x'})
     ax[0].set_xticks([])
     fig.suptitle('Boxplots')
-    ax[0].set_title('Geral')
-    ax[1].set_title('Condicionado pelas classes de saída')
+    ax[0].set_title(ax_title[0])
+    ax[1].set_title(ax_title[1])
     ax[0].grid(lw=.5, axis='y')
     ax[1].grid(lw=.5, axis='y')
     if save:
         fig.savefig(path, dpi=300)
-    plt.show()
+    if plot:
+        plt.show()
+    plt.close()
 
-def plot_bar_with_percent(df, feature_name, save=False, path='barplot.png'):
+def barplot_with_percent(df, feature_name, save=False, path='barplot.png', plot_language='pt-br', plot=True):
     """
     Plota dois gráficos de barras personalizados: 1) frequência da variável categórica em um geral e 2) condicionada pela classe de saída.
 
@@ -129,6 +145,17 @@ def plot_bar_with_percent(df, feature_name, save=False, path='barplot.png'):
         save (bool, optional): Indica se o gráfico de barras deve ser salvo;
         path (str, optional): Caminho completo de armazenamento.
     """
+    if plot_language=='pt-br':
+        ax_ylabel = 'Frequência'
+        fig_title = 'Distribuição de Categorias'
+        ax_title = ['Geral', 'Condicionadas pela classe da saída']
+    elif plot_language=='en':
+        ax_ylabel = 'Frequency'
+        fig_title = 'Category Distribution'
+        ax_title = ['General', 'Conditioned by the output classes']
+    else:
+        raise NotImplementedError
+    
     contingency_table = stats.contingency.crosstab(df.loc[:, feature_name], df.loc[:, 'Churn'])
     class_order = contingency_table.elements[0]
     target_class_percent = np.round(contingency_table.count / contingency_table.count.sum(axis=1).reshape(-1, 1) * 100, 1)
@@ -140,20 +167,22 @@ def plot_bar_with_percent(df, feature_name, save=False, path='barplot.png'):
     ax[0].bar_label(ax[0].containers[0], labels=class_percent, size=10)
     ax[1].bar_label(ax[1].containers[0], labels=target_class_percent[:, 0], size=10)
     ax[1].bar_label(ax[1].containers[1], labels=target_class_percent[:, 1], size=10)
-    ax[0].set_ylabel('Frequência')
-    ax[1].set_ylabel('Frequência')
+    ax[0].set_ylabel(ax_ylabel)
+    ax[1].set_ylabel(ax_ylabel)
     ax[0].set_xlabel(feature_name)
     ax[1].set_xlabel(feature_name)
-    fig.suptitle('Distribuição de Categorias')
-    ax[0].set_title('Geral')
-    ax[1].set_title('Condicionadas pela classe da saída')
+    fig.suptitle(fig_title)
+    ax[0].set_title(ax_title[0])
+    ax[1].set_title(ax_title[1])
     ax[0].grid(lw=.5, axis='y')
     ax[1].grid(lw=.5, axis='y')
     if save:
         fig.savefig(path, dpi=300)
-    plt.show()
+    if plot:
+        plt.show()
+    plt.close()
 
-def plot_bar_with_percent_target(df, save=False, path='barplot.png'):
+def barplot_with_percent_target(df, save=False, path='barplot.png', plot_language='pt-br', plot=True):
     """
     Plota gráfico de barras de frequência para a variável de saída 'Churn', personalizado.
 
@@ -162,6 +191,15 @@ def plot_bar_with_percent_target(df, save=False, path='barplot.png'):
         save (bool, optional): Indica se o gráfico de barras deve ser salvo;
         path (str, optional): Caminho completo de armazenamento.
     """
+    if plot_language=='pt-br':
+        ax_ylabel = 'Frequência'
+        ax_title = 'Distribuição de Categorias'
+    elif plot_language=='en':
+        ax_ylabel = 'Frequency'
+        ax_title = 'Category Distribution'
+    else:
+        raise NotImplementedError
+    
     target = df.loc[:, 'Churn']
     class_order = target.unique()
     class_percent = np.round(target.value_counts() / target.size * 100, 1)
@@ -169,9 +207,11 @@ def plot_bar_with_percent_target(df, save=False, path='barplot.png'):
     fig, ax = plt.subplots(1, 1, figsize=(5, 4), layout='constrained')
     sns.countplot(ax=ax, x=target, order=class_order, stat='count', color='gray', width=.4)
     ax.bar_label(ax.containers[0], labels=class_percent, size=10)
-    ax.set_ylabel('Frequência')
-    ax.set_title('Distribuição de Categorias')
+    ax.set_ylabel(ax_ylabel)
+    ax.set_title(ax_title)
     ax.grid(lw=.5, axis='y')
     if save:
         fig.savefig(path, dpi=300)
-    plt.show()
+    if plot:
+        plt.show()
+    plt.close()
